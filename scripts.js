0d3cef7a1c95220c4ea1a8169c13bf5ea365e697
Katarzyna Lachcik <kaisek@poczta.fm>
2017-05-29 20:03:58 +0100

    my first commit

---
+++ scripts.js
@@ -0,0 +1,13 @@
+// scripts.js
+
+var name = prompt('Enter your name');
+// alert('Hi, ' + name); komentarz lini alert
+console.log('Hi, ' + name);
+
+// to jest komentarz liniowy
+
+/*
+a to jest
+komentarz
+blokowy
+*/
